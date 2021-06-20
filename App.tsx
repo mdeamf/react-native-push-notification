eebd7bd5b36652286be46750fd4960e0ee3c580f
mdeamf <mauriciodealmeidamf@gmail.com>
2021-06-19 23:05:01 -0300

    feat: adding push notification listeners

--- App.tsx
+++ App.tsx
@@ -10,13 +10,79 @@
 
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+
+type Nullable<T> = T | undefined | null;
+interface Notification {
+  title?: Nullable<string>;
+  body?: Nullable<string>;
+  origin?: Nullable<string>;
+  data?: Nullable<object>;
+}
 
 const App = () => {
+  const [myNotif, setMyNotify] = React.useState<Notification>({});
+
+  React.useEffect(() => {
+    messaging()
+      .getToken()
+      .then(token => {
+        console.log('getToken', token);
+      });
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log('App was on the background', remoteMessage.notification);
+
+      setMyNotify({
+        title: remoteMessage.notification?.title,
+        body: remoteMessage.notification?.body,
+        origin: 'onNotificationOpenedApp',
+        data: remoteMessage.data,
+      });
+    });
+
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log('App was closed', remoteMessage.notification);
+
+          setMyNotify({
+            title: remoteMessage.notification?.title,
+            body: remoteMessage.notification?.body,
+            origin: 'getInitialNotification',
+            data: remoteMessage.data,
+          });
+        }
+      });
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('App was on the foreground', remoteMessage);
+      setMyNotify({
+        title: remoteMessage.notification?.title,
+        body: remoteMessage.notification?.body,
+        origin: 'onMessage',
+        data: remoteMessage.data,
+      });
+    });
+
+    return () => {
+      unsubscribe();
+      messaging().onTokenRefresh(token => {
+        console.log('onTokenRefresh', token);
+      });
+    };
+  }, []);
+
   return (
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Text>Hello Push Notifications!</Text>
+        <Text>Title: {myNotif.title}</Text>
+        <Text>Body: {myNotif.body}</Text>
+        <Text>Origin: {myNotif.origin}</Text>
+        <Text>Data: {JSON.stringify(myNotif.data)}</Text>
       </ScrollView>
     </SafeAreaView>
   );
